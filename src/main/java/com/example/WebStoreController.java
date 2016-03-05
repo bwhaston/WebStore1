8e7766d0c19fa5ff764324f0efbb8e0d3d1ea883
PDFormat <hastonb@students.wwu.edu>
2016-03-04 16:39:03 -0800

    Got a basic web page active displaying dummy data (an int array.) Removed JPA dependencies from pom.xml because JPA was causing errors preventing the build from succeeding.

--- src/main/java/com/example/WebStoreController.java
+++ src/main/java/com/example/WebStoreController.java
@@ -1,11 +1,17 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebStoreController
 {
-    
+    int[] nums = {1, 2, 3, 4, 5};
+
+    @RequestMapping("/items")
+    public int[] showNums()
+    {
+        return this.nums;
+    }
 }
