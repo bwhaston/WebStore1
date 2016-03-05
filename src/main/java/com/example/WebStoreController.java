dd67b221ab6df8509a97250cb8400694543e2bf7
PDFormat <hastonb@students.wwu.edu>
2016-03-05 14:46:52 -0800

    Updated WebStoreController.java to add Products to HashSet of Orders and show the Orders.

--- src/main/java/com/example/WebStoreController.java
+++ src/main/java/com/example/WebStoreController.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.util.HashSet;
+import java.util.Scanner;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,22 @@ public class WebStoreController
 {
     HashSet<Product> products = new HashSet<Product>();
 
-
+    HashSet<Order> orders = new HashSet<Order>();
 
     @RequestMapping("/items")
     public HashSet<Product> getItems()
     {
+        Product p = new Product("iMac", "This is a really nice computer", 10, 20);
+        this.products.add(p);
         return this.products;
+    }
+
+    @RequestMapping("/orders")
+    public HashSet<Order> getOrders()
+    {
+        Product p1 = new Product("Windows Box", "This is an average computer", 25, 35);
+        Order o = new Order(200, p1);
+        this.orders.add(o);
+        return this.orders;
     }
 }
