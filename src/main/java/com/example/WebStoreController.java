5d427894e34feb3279adc9bc0e46d250783409ef
PDFormat <hastonb@students.wwu.edu>
2016-03-06 10:10:52 -0800

    No longer importing java.util.Scanner

--- src/main/java/com/example/WebStoreController.java
+++ src/main/java/com/example/WebStoreController.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.util.HashSet;
-import java.util.Scanner;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
